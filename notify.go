e9c2f0e96dd113e67e96b44299ef3474ff20d10e
soyacen <soyacen@outlook.com>
2022-11-11 18:34:14 +0800

    add notify method

--- notify.go
+++ notify.go
@@ -7,43 +7,51 @@ import (
 	"github.com/go-leo/netx/httpx"
 )
 
-type SendResp struct {
-	BaseResp
+type NotifyResp struct {
+	ErrNo   int64  `json:"err_no"`
+	ErrTips string `json:"err_tips"`
 }
 
-// DataItem 模版内某个 .DATA 的值
-type DataItem struct {
-	Value any    `json:"value"`
-	Color string `json:"color"`
-}
-
-// Msg 订阅消息请求参数
-type Msg struct {
-	ToUser           string               `json:"touser"`            // 必选，接收者（用户）的 openid
-	TemplateID       string               `json:"template_id"`       // 必选，所需下发的订阅模板id
-	Page             string               `json:"page"`              // 可选，点击模板卡片后的跳转页面，仅限本小程序内的页面。支持带参数,（示例index?foo=bar）。该字段不填则模板无跳转。
-	Data             map[string]*DataItem `json:"data"`              // 必选, 模板内容
-	MiniProgramState string               `json:"miniprogram_state"` // 可选，跳转小程序类型：developer为开发版；trial为体验版；formal为正式版；默认为正式版
-	Lang             string               `json:"lang"`              // 入小程序查看”的语言类型，支持zh_CN(简体中文)、en_US(英文)、zh_HK(繁体中文)、zh_TW(繁体中文)，默认为zh_CN
+// NotifyReq 订阅消息请求参数
+type NotifyReq struct {
+	AccessToken string            `json:"access_token"` // 必选, 小程序 access_token
+	AppID       string            `json:"app_id"`       // 必选, 小程序的 id
+	TplID       string            `json:"tpl_id"`       // 必选, 模板的 id
+	OpenID      string            `json:"open_id"`      // 必选, 接收消息目标用户的 open_id
+	Data        map[string]string `json:"data"`         // 必选, 模板内容，格式形如 { "key1": "value1", "key2": "value2" }，具体使用方式参考下文请求示例
+	Page        string            `json:"page"`         // 可选, 跳转的页面
 }
 
 // Notify 发送订阅消息
-func (sm *SDK) Notify(ctx context.Context, accessToken string, msg *Msg) (*SendResp, error) {
-	var resp SendResp
+func (sdk *SDK) Notify(ctx context.Context, accessToken, tplId, openID, page string, data map[string]string) (*NotifyResp, error) {
+	req := NotifyReq{
+		AccessToken: accessToken,
+		AppID:       sdk.AppID,
+		TplID:       tplId,
+		OpenID:      openID,
+		Data:        data,
+		Page:        page,
+	}
+	var resp NotifyResp
 	err := httpx.NewRequestBuilder().
 		Post().
-		URLString("").
-		Query("access_token", accessToken).
-		JSONBody(msg).
-		Execute(ctx, sm.HttpCli).
+		URLString(sdk.getNotifyURL()).
+		JSONBody(&req).
+		Execute(ctx, sdk.HttpCli).
 		JSONBody(&resp)
 	if err != nil {
 		return nil, err
 	}
-	if resp.ErrCode != 0 {
-		err = fmt.Errorf("subscribeMessage.Send error : errcode=%v , errmsg=%v", resp.ErrCode, resp.ErrMsg)
+	if resp.ErrNo != 0 {
+		err = fmt.Errorf("notify error : errcode=%d , errmsg=%s", resp.ErrNo, resp.ErrTips)
 		return nil, err
 	}
-	resp.AppID = sm.AppID
 	return &resp, nil
+}
+
+func (sdk *SDK) getNotifyURL() string {
+	if sdk.IsSandBox {
+		return "https://open-sandbox.douyin.com/api/apps/subscribe_notification/developer/v1/notify"
+	}
+	return "https://developer.toutiao.com/api/apps/subscribe_notification/developer/v1/notify"
 }
---
+++ notify_test.go
@@ -0,0 +1,36 @@
+package douyin
+
+import (
+	"context"
+	"testing"
+
+	"github.com/go-leo/netx/httpx"
+	"github.com/go-redis/redis/v8"
+	"github.com/go-redsync/redsync/v4"
+	"github.com/go-redsync/redsync/v4/redis/goredis/v8"
+)
+
+func TestNotify(t *testing.T) {
+	appID := ""
+	secret := ""
+	client := redis.NewUniversalClient(&redis.UniversalOptions{
+		Addrs:    []string{""},
+		Password: "",
+	})
+	sdk := &SDK{
+		HttpCli:        httpx.PooledClient(),
+		AppID:          appID,
+		Secret:         secret,
+		RedisCli:       client,
+		RedisSync:      redsync.New(goredis.NewPool(client)),
+		TokenKey:       "douying:access:token:" + appID,
+		TokenLockerKey: "douying:access:token:locker" + appID,
+		Logger:         DefaultLogger{},
+		IsSandBox:      true,
+	}
+	token, err := sdk.GetToken(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(token.Data.AccessToken)
+}
